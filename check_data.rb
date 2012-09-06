7969267d9bc283c181f94511f6d2404e03716d6a
Todd Trimble <todd.trimble@gmail.com>
2012-09-05 21:08:56 -0400

    Add team name to output

--- check_data.rb
+++ check_data.rb
@@ -9,14 +9,14 @@ CONSUMER_SECRET = '442944ace4b54fddae26727e6d69c136'
 
 DATA = JSON.parse(File.read(File.join(File.dirname(__FILE__), 'users.json')))
 
-def get_data(token, secret, user_id)
+def get_data(token, secret, user_id, team)
   client = Fitgem::Client.new({:consumer_key => CONSUMER_KEY, :consumer_secret => CONSUMER_SECRET, :token => token, :secret => secret, :user_id => user_id})
   access_token = client.reconnect(token, secret)
-  {name: client.user_info['user']['fullName'], steps: client.activities_on_date(Date.today)['summary']['steps']}
+  {name: client.user_info['user']['fullName'], steps: client.activities_on_date(Date.today)['summary']['steps'], team: team}
 end
 
-data = DATA.map {|u| get_data(u['token'], u['secret'], u['user_id']) }
+data = DATA.map {|u| get_data(u['token'], u['secret'], u['user_id'], u['team'])}
 
-data.each {|u| puts "#{u[:name]}   #{u[:steps]}"}
+data.each {|u| puts "#{u[:team]}  #{u[:name]}   #{u[:steps]}"}
 puts "Total: #{data.map{|u| u[:steps]}.inject(0){|s, t| s+t}}"
 
