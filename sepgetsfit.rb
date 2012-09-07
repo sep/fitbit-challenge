988ea564a33c27ee95bcf0161a239276b0394fb6
Jon Fuller <jcfuller@sep.com>
2012-09-07 14:58:59 -0400

    added member contribution endpoint

--- sepgetsfit.rb
+++ sepgetsfit.rb
@@ -18,7 +18,7 @@ get '/team_leaderboard' do
   # TODO: optimize.  run sql in database
   activities = Activity.all
   by_team = activities.group_by{|a| a.team}
-  cnt_by_team = by_team.keys.inject([]){|memo, team_name| memo << {team: team_name, steps: by_team[team_name].inject(0){|sum, a| sum+a.steps}}}
+  cnt_by_team = by_team.keys.inject([]){|memo, team_name| memo << {team: team_name, steps: sum_steps(by_team[team_name])}}
   cnt_by_team.sort_by{|s| s[:steps]}.reverse.to_json
 end
 
@@ -26,6 +26,23 @@ get '/personal_leaderboard' do
   # TODO: optimize.  run sql in database
   activities = Activity.all
   by_person = activities.group_by{|a| a.user_id}
-  cnt_by_person = by_person.keys.inject([]){|memo, user_id| memo << {person: by_person[user_id].first.name, steps: by_person[user_id].inject(0){|sum, a| sum+a.steps}}}
+  cnt_by_person = by_person.keys.inject([]){|memo, user_id| memo << {person: by_person[user_id].first.name, steps: sum_steps(by_person[user_id])}}
   cnt_by_person.sort_by{|s| s[:steps]}.reverse.to_json
+end
+
+get '/member_contributions' do
+  activities = Activity.all
+  by_team = activities.group_by{|a| a.team}
+
+  overall = []
+  cnt_by_team = by_team.keys.each do |team_name|
+    member_data = by_team[team_name].group_by{|t| t.user_id}
+
+    overall << {team: team_name, members: member_data.keys.map{|user_id| {name: user_id, steps: sum_steps(member_data[user_id])}}}
+  end 
+  overall.sort_by{|d| d[:team]}.to_json
+end
+
+def sum_steps(enum)
+  enum.inject(0){|sum, a| sum+a.steps}
 end
