44f696ef3cb539ceecabc7e7290648952a845c23
Jon Fuller <jcfuller@sep.com>
2012-09-17 11:10:17 -0400

    robustify a little more the data updating

--- bin/archive_data.rb
+++ bin/archive_data.rb
@@ -17,12 +17,11 @@ DataMapper.auto_upgrade!
 
 DATA = JSON.parse(File.read(File.join(File.dirname(__FILE__), '../users.json')))
 
-def store(token, secret, user_id, date, team, sep_userid)
+def get_activity(token, secret, user_id, date, team, sep_userid)
   client = FitData.new(CONSUMER_KEY, CONSUMER_SECRET)
   activity = client.get_data(token, secret, user_id, date)
   activity.team = team
   activity.name ||= sep_userid
-  activity.save
 end
 
 days_ago = ARGV.first.to_i || 0
@@ -38,9 +37,10 @@ end
 DATA.select{|u| !(u['token'].empty?)}.each do |u|
   begin
     puts u['sep_userid'];
+    activity = get_activity(u['token'], u['secret'], u['user_id'], data_date, u['team'], u['sep_userid'])
     Activity.all(:date => data_date, :user_id => u['user_id']).destroy
-    store(u['token'], u['secret'], u['user_id'], data_date, u['team'], u['sep_userid'])
-  rescue
-    $stderr.puts "errored on #{u['sep_userid']}"
+    activity.save
+  rescue Exception => e
+    $stderr.puts "errored on #{u['sep_userid']}" + e.to_s
   end
 end
